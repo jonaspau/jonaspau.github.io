1b575dc46c6bb1766239c97de9625150a24081d7
Jonas Paulsen <jonaspau@outlook.com>
2024-05-22 13:06:19 +0200

    Reverted bootstrap

--- bootstrap.js
+++
@@ -1,9 +0,0 @@
-function loadBootstrap() {
-    fetch('bootstrap.html')
-        .then(response => response.text())
-        .then(data => {
-            document.write(data);
-        });
-}
-
-document.addEventListener('DOMContentLoaded', loadBootstrap);
